be77654449a2193a984d7c8788ffe0034cdda526
Valerie Racine <vracine21@gmail.com>
2024-08-29 12:17:35 -0700

    add empty homepage

--- todos.js
+++ todos.js
@@ -1,26 +1,25 @@
-// const express = require("express");
-// const morgan = require("morgan");
+const express = require("express");
+const morgan = require("morgan");
 
-// const app = express();
-// const host = "localhost";
-// const port = 3000;
+const app = express();
+const host = "localhost";
+const port = 3000;
 
-// //static data for initial testing (not persistent)
-// let todoLists = require("./lib/seed-data");
+//static data for initial testing (not persistent)
+let todoLists = require("./lib/seed-data");
 
-// app.set("views", "./views");
-// app.set("view engine", "pug");
+app.set("views", "./views");
+app.set("view engine", "pug");
 
-// app.use(morgan("common"));
-// app.use(express.static("public"));
+app.use(morgan("common"));
+app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//   res.render("lists");
-// })
+app.get("/", (req, res) => {
+  res.render("lists");
+})
 
-// //Listener
-// app.listen(port, host, () => {
-//   console.log(`Todos is listening on port ${port} of ${host}!`);
-// });
+//Listener
+app.listen(port, host, () => {
+  console.log(`Todos is listening on port ${port} of ${host}!`);
+});
 
-module.exports = [];
